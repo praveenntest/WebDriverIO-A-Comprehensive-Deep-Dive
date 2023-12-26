dd4973d58c905074f0c90d65b24718115db752ae
praveentestvagrant <praveenn@testvagrant.com>
2023-12-26 12:18:01 +0530

    handling iframe

---
+++ test/specs/iframeManagement.e2e.js
@@ -0,0 +1,9 @@
+describe('Tutorialspoint application', function(){
+    it('Frames', function(){   
+        browser.url('https://the-internet.herokuapp.com/nested_frames') 
+        browser.switchToFrame($("frame[name='frame-bottom']"))
+        const p = $('<body>')
+        console.log(p.getText() + ' - Text inside frame')
+        browser.switchToFrame(null)          
+    })
+})
