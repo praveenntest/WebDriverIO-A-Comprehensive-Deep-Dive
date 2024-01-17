07b4f671b9ff117ba623a75f4b50f2f6efa0c55e
praveentestvagrant <praveenn@testvagrant.com>
2024-01-17 17:56:17 +0530

    report

--- wdio.conf.js
+++ wdio.conf.js
@@ -111,7 +111,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: [],
+   // services: ['selenium-standalone'],
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
@@ -140,7 +140,14 @@ exports.config = {
 //     ],
 // ],
 
-reporters : ['spec'],
+// reporters : ['spec'],
+reporters: [
+    'dot',
+    ['junit', {
+        outputDir: './report'
+    }]
+],
+
 cucumberOpts: {
     require: ['./step-definitions/websiteTitleSteps.js'], // add your new file here
     //...
